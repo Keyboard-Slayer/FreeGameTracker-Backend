eedee757ab3966915c0cf1b483fcb5724ce38cb7
0v3rl0w <dalcjor@libmail.eu>
2019-08-26 16:47:26 +0000

    First commit

---
+++ include/ubisoft.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from urllib.request import (
+    Request,
+    urlopen
+)
+
+import json
+
+def get_json() -> str:
+    feed_link = ""
+    app_id = ""
+    free_game = []
+
+    data = urlopen("https://free.ubisoft.com/configuration.js").read()
+    for line in data.decode().split('\n'):
+        if 'prod' in line and 'https' in line:
+            feed_link = ':'.join(line.split(':')[-2:]).replace('\'', '')
+        if 'appId' in line:
+            app_id = line.split(':')[-1].replace('\'', '')[:-1]
+
+    req = Request(feed_link)
+    req.add_header('ubi-localecode', 'en-US')
+    req.add_header('ubi-appid', app_id)
+
+
+    for game in json.loads(urlopen(req).read().decode())['news']:
+        if game['type'] == "freegame":
+            free_game.append({"name": game["body"],
+                              "expiration": game["expirationDate"],
+                              "mediaURL": game["mediaURL"],
+                              "publicationDate": game["publicationDate"],
+                              "link": game["links"][0]["param"]})
+    return free_game
---
+++ main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+from flask import (
+    Flask,
+    jsonify,
+    render_template
+)
+
+app = Flask(__name__)
+
+def init() -> dict:
+    modules = {}
+    for module in os.listdir("./include"):
+        if module.split('.')[-1] == "py" and module != "__init__.py":
+            exec("import include.{0} as {0}".format('.'.join(module.split('.')[:-1])))
+            modules['.'.join(module.split('.')[:-1])] = eval(f"{'.'.join(module.split('.')[:-1])}")
+    return modules
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/api/')
+def api():
+    data = [module.get_json() for module in globals()['modules'].values()]
+    return jsonify(data)
+
+if __name__ == "__main__":
+    globals()['modules'] = init()
+    app.run()
